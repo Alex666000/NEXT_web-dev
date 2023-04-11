cb194877d6ded20bc66cdffa92e17fcb50e30e17
Aleksandr_UB <bogdanov777000@mail.ru>
2023-04-11 19:14:15 +0300

    getStaticProps и обработали ошибку асинхронного запроса

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,8 +1,8 @@
 import { useEffect } from "react";
 import { useRouter } from "next/router";
 import Head from "next/head";
-import Heading from "../components/Heading";
 import styles from "../styles/404.module.scss";
+import Heading from "@/components/Headind";
 
 const Error = () => {
     const router = useRouter();
--- src/pages/contacts/index.js
+++ src/pages/contacts/index.js
@@ -1,20 +1,43 @@
-import React from "react";
-import Heading from "@/components/Headind";
 import Head from "next/head";
+import Heading from "@/components/Headind";
 
 
-// теперь при переходе на роут http://localhost:3000/contacts у нас отобразится общая страница контактов
-const Contacts = () => {
+//наше приложение осуществляет теперь запрос за данными прямо на сервере и возвращает нам полностью готовую разметку
+//
+export const getStaticProps = async () => {
+    const response = await fetch('https://jsonplaceholder.typicode.com/users');
+    // const data = await response.json();
+    const data = null;
+
+    // когда работаем с сервер данные могут не прийти - делаем проверку
+    if (!data) {
+        return {
+            // если  данные не существуют вернем спец.объект который перехватывается и обрабатывается в Next
+            // в этом случае вернется 404 ошибка и соответствующая страница
+            // так мы обработали ошибку асинхронного запроса
+            notFound: true,
+        }
+    }
+
+    return {
+        props: { contacts: data },
+    }
+};
+
+const Contacts = ({ contacts }) => {
     return (
         <>
             <Head>
                 <title>Contacts</title>
             </Head>
-            <Heading text={"Contacts list"}></Heading>
+            <Heading text="Contacts list:" />
+            <ul>
+                {contacts && contacts.map(({ id, name, email }) => (
+                    <li key={id}><strong>{name}</strong> ({email})</li>
+                ))}
+            </ul>
         </>
-
     );
 };
+
 export default Contacts;
-
-
